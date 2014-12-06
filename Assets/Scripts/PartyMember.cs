68d97b309e0b8c25b350ab75645f2758db6779f6
Joseph Utecht <thenmal@gmail.com>
2014-12-06 14:43:19 -0600

    cooldowns working

--- Assets/Scripts/PartyMember.cs
+++ Assets/Scripts/PartyMember.cs
@@ -6,6 +6,7 @@ public class PartyMember : MonoBehaviour {
 	public Person me;
 	public Toggle special_toggle;
 	public Toggle attack_toggle;
+	public Text cooldown;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,8 +16,11 @@ public class PartyMember : MonoBehaviour {
 	void Update () {
 		special_toggle.interactable = me.current_delay == 0;
 		if(special_toggle.interactable == false) {
+			cooldown.text = me.current_delay.ToString();
 			attack_toggle.isOn = true;
 			special_toggle.isOn = false;
+		} else {
+			cooldown.text = "";
 		}
 	}
 }
